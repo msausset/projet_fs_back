52146747c99c83b9dc65356ed83ce0f0847684fd
Cynthia <cynthiazn.rabe@gmail.com>
2022-10-25 23:27:23 +0200

    Affichage des articles choisis dans le panier

--- app-front/src/components/Cart.js
+++ app-front/src/components/Cart.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -5,10 +7,40 @@ import Col from 'react-bootstrap/Col';
 import '../Cart.css';
 
 export default function Cart() {
+
+    const [ panier, setPanier ] = useState([])
+
+    useEffect(() => {
+
+        const articles = JSON.parse(localStorage.getItem('panier'))
+
+        if(articles) {
+            setPanier(articles)
+        }
+
+    }, [])
+
     return (
         <Container fluid="md">
             <Row>
                 <Col><h1>Mon panier</h1></Col>
+            </Row>
+            <Row>
+                <Col>
+                
+                {
+                    panier.map(article => (
+
+                        <ul key={ article.id }>
+                            <li>{ article.name }</li>
+                            <li>{ article.price + ' x ' + article.qte }</li>
+                        </ul>
+
+                    ))
+
+                }
+                
+                </Col>
             </Row>
         </Container>
     )
